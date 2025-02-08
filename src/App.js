8a6fe4de4b3c3f3fc47456f3b1f1079c5309c7b9
r-s-coding <112776326+r-s-coding@users.noreply.github.com>
2025-02-08 18:03:44 -0500

    Added fetching and displaying API data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+import React, { useState } from 'react';
 import NavBar from './components/NavBar';
 import WeatherDisplay from './components/WeatherDisplay';
 import Footer from './components/Footer';
 
+
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);  
+
   return (
     <div className="App">
-      <NavBar />
-      <WeatherDisplay />
+      <NavBar weather={setWeatherData} err={setError} />
+      {weatherData && <WeatherDisplay data={weatherData} />}
       <Footer />
     </div>
   );
--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,15 +1,16 @@
 import React from "react";
 
-function WeatherDisplay() {
-  return (
-    <div className="WeatherDisplay">
-      <h1>20 Deg C</h1>
-      <h2>Location: Toronto</h2>
-      <p>Humidity: 60%</p>
-      <p>Wind Speed: 10 km/hr</p>
-      <p>Weather: Sunny</p>
-    </div>
-  );
+function WeatherDisplay( {data} ) {
+    console.log(data)
+    return (
+        <div className="WeatherDisplay">
+            <h1>{data.main.temp} &#176;C</h1>
+            <h2>Location: {data.name}</h2>
+            <p>Humidity: {data.main.humidity}%</p>
+            <p>Wind Speed: {data.wind.speed} m/s</p>
+            <p>Weather: {data.weather[0].description}</p>
+        </div>
+    );
 }
 
 export default WeatherDisplay;
