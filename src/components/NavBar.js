2dc57c74bb5e144c83941d670971effedc73cc75
r-s-coding <112776326+r-s-coding@users.noreply.github.com>
2025-02-08 16:07:57 -0500

    Created the three main component files - navbar, weatherdisplay, footer

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <h1>Weather App</h1>
+    </div>
+  );
+}
+
+export default NavBar;
---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function WeatherDisplay() {
+  return (
+    <div className="WeatherDisplay">
+      <h1>20 Deg C</h1>
+      <h2>Location: Toronto</h2>
+      <p>Humidity: 60%</p>
+      <p>Wind Speed: 10 km/hr</p>
+      <p>Weather: Sunny</p>
+    </div>
+  );
+}
+
+export default WeatherDisplay;
